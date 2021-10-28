fb63eaacd8ea4bfd5445e321a5f72299a7449f5c
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2021-10-28 21:35:27 +0500

    deploying

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import serializers
+from rest_framework.generics import *
+from .serializers import *
 # Create your views here.
+
+class CarView(ListCreateAPIView):
+    serializer_class = CarSerializer
+    queryset = Car.objects.all()
