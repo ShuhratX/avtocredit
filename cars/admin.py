3fe803bad80b99fdfd73a4181a7cf99a87b6a453
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2021-10-29 17:09:08 +0500

    admin

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+admin.site.register(CarModel)
+admin.site.register(Complectation)
+admin.site.register(Car)
+admin.site.register(Credit)
+admin.site.register(Calculator)
