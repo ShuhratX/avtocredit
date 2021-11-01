67f4fa5057c18cb1ccf43a3fc67c17cda888fee6
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2021-11-01 22:13:52 +0500

    compare

--- cars/urls.py
+++ cars/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('models/', CarModelView.as_view()),
     path('credit/', CreditView.as_view()),
     path('compare/', ComparisonView.as_view()),
+    path('calculator/', CalculatorView.as_view()),
 
     
 ]
--- cars/views.py
+++ cars/views.py
@@ -13,20 +13,20 @@ class CarModelView(ListCreateAPIView):
     queryset = CarModel.objects.all()
 
 
-class CarView(ListCreateAPIView):
+class CarView(ListAPIView):
     serializer_class = CarSerializer
     queryset = Car.objects.all()
     filter_backends = [django_filters.rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['complectation',]
-    filter_fields = ['color', 'price',]
+    filter_fields = ['color', 'price', 'fuel_type',]
 
 
-class CreditView(ListCreateAPIView):
+class CreditView(ListAPIView):
     serializer_class = CreditSerializer
     queryset = Credit.objects.all()
 
 
-class CalculatorView(ListCreateAPIView):
+class CalculatorView(CreateAPIView):
     serializer_class = CalculatorSerializer
     queryset = Calculator.objects.all()
 
@@ -42,20 +42,20 @@ class ComparisonView(GenericAPIView):
         if car1 and car2:
             comps = [car1, car2]
             for comp in comps:
-                car = Car.objects.filter(complectation=comp)
-                if car:
-                    cars.append({
-                        "model": car.model,
-                        "complectation": car.complectation,
-                        "price": car.price,
-                        "color": car.color,
-                        "power": car.power,
-                        "fuel_type": car.fuel_type,
-                        "transmission": car.transmission,
-                        "top_speed": car.top_speed,
-                    })
-                else:
-                    cars = ["Mavjud bo'lmagan avto"]
+                for car in Car.objects.filter(complectation=comp):
+                    if car:
+                        cars.append({
+                            "model": car.model.name,
+                            "complectation": car.complectation,
+                            "price": car.price,
+                            "color": car.color,
+                            "power": car.power,
+                            "fuel_type": car.fuel_type,
+                            "transmission": car.transmission,
+                            "top_speed": car.top_speed,
+                        })
+                    else:
+                        cars = ["Mavjud bo'lmagan avto"]
             return Response(cars)
         else:
             return Response("Ikkita mashina tanlang!")
